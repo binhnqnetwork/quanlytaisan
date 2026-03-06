25739a75c815369b73bf3ca0b59b2ffde4ce7bbe
binhnqnetwork <binhnq.network@gmail.com>
2026-03-06 08:24:03 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,20 @@
+import streamlit as st
+from cryptography.fernet import Fernet
 import hashlib
 import base64
-from cryptography.fernet import Fernet
 
 def get_cipher():
-    # Bước này cực quan trọng: Biến "4Oqlts" thành 32-byte key chuẩn xác
-    user_key = st.secrets["ENCRYPTION_KEY"]
-    key_32bytes = hashlib.sha256(user_key.encode()).digest()
-    fernet_key = base64.urlsafe_b64encode(key_32bytes)
-    return Fernet(fernet_key)
+    # Biến key "4Oqlts" thành 32-byte key chuẩn
+    raw_key = st.secrets["ENCRYPTION_KEY"]
+    hashed_key = hashlib.sha256(raw_key.encode()).digest()
+    return Fernet(base64.urlsafe_b64encode(hashed_key))
+
+def encrypt_pw(password):
+    if not password: return ""
+    return get_cipher().encrypt(password.encode()).decode()
+
+def decrypt_pw(encrypted_text):
+    try:
+        return get_cipher().decrypt(encrypted_text.encode()).decode()
+    except:
+        return "❌ Sai Key hoặc lỗi dữ liệu"
